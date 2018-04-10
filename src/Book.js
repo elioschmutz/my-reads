e749f359d022c6cea51fc8ebb285f2c67237579b
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-04-10 21:40:54 +0200

    feat: Implement Book component

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Book.css';
 
 class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired
+  };
+
   render() {
-    return <div className="book" />;
+    const { book } = this.props;
+
+    return (
+      <div className="book">
+        <div className="card">
+          <div
+            className="card-img-top"
+            style={{
+              backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{book.title}</h5>
+            <p className="card-text">{book.authors}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Book from './Book';
 
 class Shelf extends Component {
   static propTypes = {
@@ -25,17 +26,7 @@ class Shelf extends Component {
           {books.length <= 0 && <span>No books in this shelf</span>}
           {this.props.books.map(book => (
             <div className="col-sm-4 col-md-3" key={book.id}>
-              <div className="card">
-                <img
-                  className="card-img-top"
-                  src="./dummy.png"
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Ender's Game</h5>
-                  <p className="card-text">Orson Scott Card</p>
-                </div>
-              </div>
+              <Book book={book} />
             </div>
           ))}
         </div>
