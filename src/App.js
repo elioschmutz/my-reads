881e24b89ccdec74fc02e6a2bcab82c5048473be
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-04-10 20:06:42 +0200

    feat: Add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import ListBooks from './ListBooks';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <ListBooks />
+      <div>
+        <Route exact path="/" component={ListBooks} />
       </div>
     );
   }
