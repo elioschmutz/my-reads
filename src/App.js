36fcb143d07c73f5c5e6fcca3d93df262779c067
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-04-10 20:19:58 +0200

    feat: Set books state for App component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import ListBooks from './ListBooks';
 import { Route } from 'react-router-dom';
 import './App.css';
+import * as BooksAPI from './BooksAPI';
 
 class App extends Component {
+  state = {
+    books: []
+  };
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({
+        books
+      });
+    });
+  }
   render() {
     return (
       <div>
+        {JSON.stringify(this.state.books)}
         <Route exact path="/" component={ListBooks} />
+        }
       </div>
     );
   }
